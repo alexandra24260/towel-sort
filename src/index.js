60d93d9f1245796203126847d53b58ab088836ed
alexandra.Vecher <alexandra_vecher@epam.com>
2021-03-11 11:01:01 +0300

    feat: solution for towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 
 // You should implement your task here.
 
+const { sort } = require("semver");
+
 module.exports = function towelSort (matrix) {
-  return [];
+  let output = [];
+  let result = [];
+  let valueReverse; 
+  if (!matrix){
+    return [];
+  }
+  for (const [key, value] of Object.entries(matrix)) {
+    if ( key % 2 == 0) {
+    for (const [subkey, subvalue] of Object.entries(value)) {
+        result.push(subvalue);
+    }
+  }
+    else 
+    { 
+      valueReverse = value.reverse();
+      for (const [subkey, subvalue] of Object.entries(valueReverse)) {
+        result.push(subvalue);
+    }
+    }
+  }
+  return result;
 }
